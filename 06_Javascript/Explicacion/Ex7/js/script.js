cab7f12e11e2c4309a3607ac9b0ea54b3a343fa2
Martin Gil Blanco <gilblancomartin@gmail.com>
2024-11-27 20:09:11 +0100

    EX7 JS refactorizado

--- 06_Javascript/Explicacion/Ex7/js/script.js
+++ 06_Javascript/Explicacion/Ex7/js/script.js
@@ -1,14 +1,24 @@
 // 14. Asincronía en JavaScript
 // 15. Promesas y Peticiones fetch y AJAX
 
+/*
+fetch es una función nativa de JavaScript que se utiliza para realizar solicitudes HTTP
+desde el navegador. Es una API moderna y más sencilla que el objeto XMLHttpRequest y
+permite trabajar con solicitudes y respuestas de recursos a través de promesas.
+*/
+
 const url_usuarios = "https://jsonplaceholder.typicode.com/users";
-const url_pokemon = "https://pokeapi.co/api/v2/pokemon";
 const url_productos = "./js/productos.json";
+const url_pokemon = "https://pokeapi.co/api/v2/pokemon";
 
 const buscar = (url, id_listado) => {
   fetch(url)
-    .then((res) => res.json())
+    .then((res) => {
+      console.log("Respuesta en crudo", res); // Objeto response
+      return res.json();
+    })
     .then((data) => {
+      console.log("Respuesta en JSON", data);
       let listado = document.querySelector(id_listado);
       data.forEach((usuario) => {
         const li = document.createElement("li");
@@ -32,5 +42,5 @@ const buscar_pokemon = (url, id_listado) => {
 };
 
 buscar(url_usuarios, "#lista_usuarios");
-buscar_pokemon(url_pokemon, "#lista_pokemon");
 buscar(url_productos, "#lista_productos");
+buscar_pokemon(url_pokemon, "#lista_pokemon");
