29113c024c22383cdb49c641b033e9e711d8d5bd
Martin Gil Blanco <gilblancomartin@gmail.com>
2024-11-27 19:36:30 +0100

    Refactorización apuntes EX4,5,6 de JS

--- 06_Javascript/Explicacion/Ex5/js/script.js
+++ 06_Javascript/Explicacion/Ex5/js/script.js
@@ -45,3 +45,29 @@ document.getElementById("crear_sesion").addEventListener("click", crear_sesion);
 document.getElementById("mostrar_sesion").addEventListener("click", mostrar_sesion);
 document.getElementById("modificar_sesion").addEventListener("click", modificar_sesion);
 document.getElementById("eliminar_sesion").addEventListener("click", eliminar_sesion);
+
+// Almacenamiento de JSON en localStorage o sessionStorage
+
+let respuestaBD = [
+  {
+    titulo: "terminator",
+    anho: 1984,
+  },
+  {
+    titulo: "armageddon ",
+    anho: 1998,
+  },
+];
+
+let respuestaBDJSON = JSON.stringify(respuestaBD);
+localStorage.setItem("respuestaBD", respuestaBD);         // [object Object],[object Object]
+localStorage.setItem("respuestaBDJSON", respuestaBDJSON); //[{"titulo":"terminator","anho":1984},{"titulo":"armageddon ","anho":1998}]
+
+console.log(localStorage.getItem("respuestaBD"));
+console.log(localStorage.getItem("respuestaBDJSON"));
+
+let respuesta = localStorage.getItem("respuestaBDJSON");
+console.log(respuesta[0].titulo); // undefined
+
+respuesta = JSON.parse(localStorage.getItem("respuestaBDJSON"));
+console.log(respuesta[0].titulo); // undefined
--- 06_Javascript/Explicacion/Ex6/js/script.js
+++ 06_Javascript/Explicacion/Ex6/js/script.js
@@ -1,17 +1,32 @@
 // 14. Asincronía en JavaScript
 // 15. Promesas y Peticiones fetch y AJAX
 
+/*
+JavaScript funciona por defecto de forma síncrona, es decir, se ejecutan las instrucciones
+una despues de otra y en caso de que algo produzca un error es bloqueante por lo que no se 
+continua con la ejecución del código.
+
+JavaScript tambien permite un funcionamiento asincrónico, es decir, si tenemos una función
+como setTimeout que ejecuta otra función una vez concluido un tiempo determinado, JS lee la
+función pero no detiene la ejecución del resto del código.
+*/
+
+console.log("Hola");
+console.log("¿Qué tal todo?");
+// console.log(noExiste);
+setTimeout(() => console.log("Asincronía"), 1000);
+// setTimeout(() => console.log(ewfrgtyh), 1000);
+setInterval(() => console.log("Ejecuta cada 2 segundos"), 2000);
+console.log("Adios");
+
 // Promesas
 
-/*const eventoFuturo = (res) => {
-  return new Promise((resolve, reject) => {
-    if (res === true) {
-      resolve("Promesa resuelta");
-    } else {
-      reject("Promesa rechazada");
-    }
-  });
-};*/
+/*
+Una promesa representa un evento a futuro como una consulta a una base de datos, no sabemos
+cuanto va a tardar la respuesta desde el servicio. Un ejemplo típico para visualizarlo es un
+ecommerce donde sabemos la estructura de los datos que vamos a obtener pero no sabemos cuando
+tendremos la respuesta (pudiendo ser esta positiva (los datos) o negativa (un 404 por ejemplo)).
+*/
 
 const insertarPeliculas = (listado) => {
   let lista = document.querySelector("#listaPelis");
@@ -20,9 +35,16 @@ const insertarPeliculas = (listado) => {
   });
 };
 
+const errorRespuesta = (res) => {
+  let lista = document.querySelector("#listaPelis");
+  lista.innerHTML += res;
+}
+
 const eventoFuturo = (res) => {
+  // Acción que a futuro devolverá una respuesta aceptada o rechazada
   return new Promise((resolve, reject) => {
     setTimeout(() => {
+
       let respuestaBD = [
         {
           titulo: "terminator",
@@ -33,22 +55,33 @@ const eventoFuturo = (res) => {
           anho: 1998,
         },
       ];
+
       res === true ? resolve(respuestaBD) : reject("Error en la respuesta");
     }, 2000);
   });
 };
 
-let valor = true;
+console.log(eventoFuturo(true));
 
-console.log(eventoFuturo(valor));
+/*
+Promise { <state>: "pending" } 
+Se ejecuta de forma síncrona sin esperar la devolución de la promesa por lo que el estado es
+pendiente.
+*/
 
-eventoFuturo(valor)
+eventoFuturo(true)
   .then((res) => {
+    console.log("Respuesta", res);
     insertarPeliculas(res);
   })
   .catch((res) => {
-    console.log(res);
+    console.log("Respuesta", res);
+    errorRespuesta(res);
   })
   .finally(() => {
     console.log("Finalizó el proceso");
-  });
+  }); 
+  
+/*
+Se ejecuta la promesa de forma asíncrona esperando la respuesta.
+*/
